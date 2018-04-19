28bdb2ef33cf0bf51780bc4aa79790651de59d9e
Eric McDowell <eric.mcdowell@kyderby.com>
2018-04-19 16:18:08 -0400

    fixing icon sizing still, updating booking info script to use css classes to facilitate different screen proportions

--- script/script.js
+++ script/script.js
@@ -2,10 +2,10 @@ var bookingExpanded = false;
 
 $("#booking").on('click', function() {
     if(!bookingExpanded) {
-        $(this).children('span').html("Phone: 502-500-8727<br>Email: maddylobeck@gmail.com").css("font-size", "36px");
+        $(this).children('span').html("Phone: 502-500-8727<br>Email: maddylobeck@gmail.com").addClass('booking-info-expanded');
         bookingExpanded = true;
     } else {
-        $(this).children('span').html("Booking Info").css("font-size", "48px");
+        $(this).children('span').html("Booking Info").removeClass('booking-info-expanded');
         bookingExpanded = false;
     }
 })
